e8881754347c37d91bc84f9bf8067828bf598067
OlgaShelkova <olgashelkova1@gmail.com>
2023-12-30 15:01:09 +0300

    fixed syntax errors

--- week16.js
+++ week16.js
@@ -1,52 +1,50 @@
 document.getElementById('registrationForm').addEventListener('submit', function (event) {
-event.preventDefault(); // Предотвращаем отправку формы по умолчанию
+	event.preventDefault(); // Предотвращаем отправку формы по умолчанию
 
-  let fields = document.getElementsByClassName('field'); // Получаем все поля ввода
-  //let isValid = true; // Устанавливаем флаг валидности формы в значение true
+	let isValid = true; // Устанавливаем флаг валидности формы в значение true
+	let fields = document.getElementsByClassName('field'); // Получаем все поля ввода
 
-  for (let i = 0; i < fields.length; i++) { // Проходимся по каждому полю в цикле
-    let field = fields[i]; // Получаем текущее поле
-    let value = field.value.trim(); // Получаем значение поля и удаляем пробелы в начале и в конце
-    let errorMessage = field.nextElementSibling; // Получаем сообщение об ошибке для данного поля
+	for (let i = 0; i < fields.length; i++) {// Проходимся по каждому полю в цикле
+		
+		let field = fields[i]; // Получаем текущее поле
+		let errorMessage = field.nextElementSibling; // Получаем сообщение об ошибке для данного поля
 
-    field.addEventListener('focus', function () { // Добавляем обработчик события фокуса для поля
-      errorMessage.innerText = ''; // При фокусе очищаем сообщение об ошибке
-    });
+		field.addEventListener('focus', function () {// Добавляем обработчик события фокуса для поля
+			
+			errorMessage.innerText = ''; // При фокусе очищаем сообщение об ошибке
+		});
 
-    field.addEventListener('blur', function () { // Добавляем обработчик события потери фокуса для поля
-      if (value === '') { // Проверяем, содержит ли поле значение
-        errorMessage.innerText = 'Это поле обязательно для заполнения'; // Если пустое, отображаем сообщение об ошибке
-        isValid = false; // Устанавливаем флаг валидности в значение false
-      } else {
-        errorMessage.innerText = ''; // Если не пустое, очищаем сообщение об ошибке
-      }
-    });
+		field.addEventListener('blur', function () {// Добавляем обработчик события потери фокуса для поля
+			
+			let value = field.value.trim(); // Получаем значение поля и удаляем пробелы в начале и в конце
+			if (value === '') {
+				// Проверяем, содержит ли поле значение
+				errorMessage.innerText = 'Это поле обязательно для заполнения'; // Если пустое, отображаем сообщение об ошибке
+				isValid = false; // Устанавливаем флаг валидности в значение false
+			}
+		});
 
-    if (value === '') { // Проверяем, содержит ли поле значение после выхода из фокуса
-      errorMessage.innerText = 'Это поле обязательно для заполнения'; // Если пустое, отображаем сообщение об ошибке
-      isValid = false; // Устанавливаем флаг валидности в значение false
-    } else {
-      errorMessage.innerText = ''; // Если не пустое, очищаем сообщение об ошибке
-    }
-  }
+		// Проверка значений полей в момент отправки формы
+		let value = field.value.trim();
+		if (value === '') {
+			errorMessage.innerText = 'Это поле обязательно для заполнения';
+			isValid = false;
+		}
+	}
+
+	let consentCheckbox = document.getElementById('consent'); // Получаем чекбокс согласия
+	let consentErrorMessage = document.getElementById('agree-error-message'); // Получаем сообщение об ошибке для чекбокса
+
+	if (!consentCheckbox.checked) {
+		// Проверяем, отмечен ли чекбокс
+		consentErrorMessage.innerText = 'Вы должны дать согласие на обработку данных'; // Если не отмечен, отображаем сообщение об ошибке
+		isValid = false; // Устанавливаем флаг валидности в значение false
+	}
+
+	if (isValid) {
+		console.log('Form Data:', {
+			fields: Array.from(fields).map((field) => field.value.trim()),
+			consentCheckbox: consentCheckbox.checked,
+		});
+	}
 });
-
-let consentCheckbox = document.getElementById('consent'); // Получаем чекбокс согласия
-let consentErrorMessage = document.getElementById('agree-error-message'); // Получаем сообщение об ошибке для чекбокса
-let isValid = true; // Устанавливаем флаг валидности формы в значение true
-
-consentCheckbox.addEventListener('change', function () { // Добавляем обработчик изменения значения чекбокса согласия
-  if (!consentCheckbox.checked) { // Проверяем, отмечен ли чекбокс
-    consentErrorMessage.innerText = 'Вы должны дать согласие на обработку данных'; // Если не отмечен, отображаем сообщение об ошибке
-    isValid = false; // Устанавливаем флаг валидности в значение false
-  } else {
-    consentErrorMessage.innerText = ''; // Если отмечен, очищаем сообщение об ошибке
-  }
-});
-//где-то здесь ошибка???
-if (isValid) {
-  console.log('Form Data:', {
-    fields: Array.from(fields).map(field => field.value.trim()),
-    consentCheckbox: consentCheckbox.checked
-  });
-}
